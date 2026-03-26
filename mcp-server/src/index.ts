671397b55bceda84abcf9934c972991b91594bce
FStevenson91 <felstefer28791@gmail.com>
2026-03-26 18:53:26 -0300

    feature: search_products tool

--- mcp-server/src/index.ts
+++ mcp-server/src/index.ts
@@ -10,6 +10,23 @@ export class MyMCP extends McpAgent {
 	});
 
 	async init() {
+
+		this.server.tool(
+			"search_products",           // 1. nombre de la tool (string)
+			{ query: z.string() },       // 2. parámetros con validación zod
+			async ({ query }) => {       // 3. función que ejecuta la lógica
+				// acá va el SELECT a D1
+				const searchTerm = `%${query}%`;
+				const result = await this.env.ecommerce_db // aca debemos configurar en worker-configuration.d.ts el binding a D1 con el nombre "ecommerce_db", en interface ENV junto al MCP_OBJECT
+					.prepare("SELECT * FROM products WHERE garment_type LIKE ? OR color LIKE ? OR category LIKE ? OR description LIKE ?")
+					.bind(searchTerm, searchTerm, searchTerm, searchTerm)
+					.all();
+					return {
+						content: [{ type: "text", text: JSON.stringify(result.results) }]
+					};
+			}
+		);
+
 		// Simple addition tool
 		this.server.tool("add", { a: z.number(), b: z.number() }, async ({ a, b }) => ({
 			content: [{ type: "text", text: String(a + b) }],
